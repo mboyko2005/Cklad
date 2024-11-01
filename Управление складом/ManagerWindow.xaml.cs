905c0fe9ee56d8c31fa7185c52ae9a9f2969ca7a
Максим Бойко <mboyko2005@gmail.com>
2024-11-01 15:27:37 +0500

    С темами прорабоатл баги( И сделал почти управление складом ошибка с товарами ID

--- Управление складом/ManagerWindow.xaml.cs
+++ Управление складом/ManagerWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace УправлениеСкладом
 {
@@ -7,6 +8,49 @@ namespace УправлениеСкладом
 		public ManagerWindow()
 		{
 			InitializeComponent();
+		}
+
+		// Метод для обработки события при нажатии на окно (перемещение окна)
+		private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if (e.ChangedButton == MouseButton.Left)
+				this.DragMove();
+		}
+
+		// Метод для обработки нажатия на кнопку "Закрыть"
+		private void CloseButton_Click(object sender, RoutedEventArgs e)
+		{
+			this.Close();
+		}
+
+		// Метод для обработки нажатия на кнопку "Переключение темы"
+		private void ToggleTheme_Click(object sender, RoutedEventArgs e)
+		{
+			// Логика переключения темы
+		}
+
+		// Метод для обработки нажатия на кнопку "Управление заказами"
+		private void ManageOrders_Click(object sender, RoutedEventArgs e)
+		{
+			// Логика управления заказами
+		}
+
+		// Метод для обработки нажатия на кнопку "Управление клиентами"
+		private void ManageClients_Click(object sender, RoutedEventArgs e)
+		{
+			// Логика управления клиентами
+		}
+
+		// Метод для обработки нажатия на кнопку "Просмотр отчетов"
+		private void ViewReports_Click(object sender, RoutedEventArgs e)
+		{
+			// Логика просмотра отчетов
+		}
+
+		// Метод для обработки нажатия на кнопку "Настройки"
+		private void Settings_Click(object sender, RoutedEventArgs e)
+		{
+			// Логика для настроек
 		}
 
 		public void ShowWindow()
--- Управление складом/WarehouseStaffWindow.xaml.cs
+++ Управление складом/WarehouseStaffWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace УправлениеСкладом
 {
@@ -7,6 +8,49 @@ namespace УправлениеСкладом
 		public WarehouseStaffWindow()
 		{
 			InitializeComponent();
+		}
+
+		// Метод для обработки события при нажатии на окно (перемещение окна)
+		private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if (e.ChangedButton == MouseButton.Left)
+				this.DragMove();
+		}
+
+		// Метод для обработки нажатия на кнопку "Закрыть"
+		private void CloseButton_Click(object sender, RoutedEventArgs e)
+		{
+			this.Close();
+		}
+
+		// Метод для обработки нажатия на кнопку "Переключение темы"
+		private void ToggleTheme_Click(object sender, RoutedEventArgs e)
+		{
+			// Логика переключения темы
+		}
+
+		// Метод для обработки нажатия на кнопку "Просмотр товаров"
+		private void ViewItems_Click(object sender, RoutedEventArgs e)
+		{
+			// Логика просмотра товаров
+		}
+
+		// Метод для обработки нажатия на кнопку "Управление складом"
+		private void ManageStock_Click(object sender, RoutedEventArgs e)
+		{
+			// Логика управления складом
+		}
+
+		// Метод для обработки нажатия на кнопку "Перемещение товаров"
+		private void MoveItems_Click(object sender, RoutedEventArgs e)
+		{
+			// Логика перемещения товаров
+		}
+
+		// Метод для обработки нажатия на кнопку "Журнал инвентаризации"
+		private void InventoryLog_Click(object sender, RoutedEventArgs e)
+		{
+			// Логика для журнала инвентаризации
 		}
 
 		public void ShowWindow()
