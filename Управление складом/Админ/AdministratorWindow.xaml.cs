219007c1cbc27ebeb1e03b0da5988ae5036e795f
Максим Бойко <mboyko2005@gmail.com>
2025-02-14 17:09:06 +0500

    Шрифт

--- Управление складом/Админ/AdministratorWindow.xaml.cs
+++ Управление складом/Админ/AdministratorWindow.xaml.cs
@@ -1,82 +1,71 @@
-﻿// AdministratorWindow.xaml.cs
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Input;
 using MahApps.Metro.IconPacks;
 using Управление_складом.Themes;
 
 namespace УправлениеСкладом
 {
-	public partial class AdministratorWindow : Window, IRoleWindow, IThemeable
-	{
-		public AdministratorWindow()
-		{
-			InitializeComponent();
-		}
+    public partial class AdministratorWindow : Window, IRoleWindow, IThemeable
+    {
+        public AdministratorWindow()
+        {
+            InitializeComponent();
+            UpdateThemeIcon();
+        }
 
-		public void ShowWindow()
-		{
-			Show();
-		}
+        public void ShowWindow() => Show();
 
-		private void CloseButton_Click(object sender, RoutedEventArgs e)
-		{
-			Application.Current.Shutdown();
-		}
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
 
-		private void ManageUsers_Click(object sender, RoutedEventArgs e)
-		{
-			ManageUsersWindow usersWindow = new ManageUsersWindow
-			{
-				Owner = this
-			};
-			usersWindow.ShowDialog(); // Открытие как модального окна
-		}
+        private void ManageUsers_Click(object sender, RoutedEventArgs e)
+        {
+            var usersWindow = new ManageUsersWindow { Owner = this };
+            usersWindow.ShowDialog();
+        }
 
-		private void ManageInventory_Click(object sender, RoutedEventArgs e)
-		{
-			ManageInventoryWindow inventoryWindow = new ManageInventoryWindow
-			{
-				Owner = this
-			};
-			inventoryWindow.ShowDialog();
-		}
+        private void ManageInventory_Click(object sender, RoutedEventArgs e)
+        {
+            var inventoryWindow = new ManageInventoryWindow { Owner = this };
+            inventoryWindow.ShowDialog();
+        }
 
-		private void Reports_Click(object sender, RoutedEventArgs e)
-		{
-			ReportsWindow reportsWindow = new ReportsWindow
-			{
-				Owner = this
-			};
-			reportsWindow.ShowDialog();
-		}
+        private void Reports_Click(object sender, RoutedEventArgs e)
+        {
+            var reportsWindow = new ReportsWindow { Owner = this };
+            reportsWindow.ShowDialog();
+        }
 
-		private void Settings_Click(object sender, RoutedEventArgs e)
-		{
-			SettingsWindow settingsWindow = new SettingsWindow
-			{
-				Owner = this
-			};
-			settingsWindow.ShowDialog();
-		}
+        private void Settings_Click(object sender, RoutedEventArgs e)
+        {
+            var settingsWindow = new SettingsWindow { Owner = this };
+            settingsWindow.ShowDialog();
+        }
 
-		private void ToggleTheme_Click(object sender, RoutedEventArgs e)
-		{
-			ThemeManager.ToggleTheme();
-			UpdateThemeIcon();
-		}
+        private void ToggleTheme_Click(object sender, RoutedEventArgs e)
+        {
+            ThemeManager.ToggleTheme();
+            UpdateThemeIcon();
+        }
 
-		public void UpdateThemeIcon()
-		{
-			if (ThemeIcon != null)
-			{
-				ThemeIcon.Kind = ThemeManager.IsDarkTheme ? PackIconMaterialKind.WeatherNight : PackIconMaterialKind.WeatherSunny;
-			}
-		}
+        public void UpdateThemeIcon()
+        {
+            if (ThemeIcon != null)
+            {
+                ThemeIcon.Kind = ThemeManager.IsDarkTheme 
+                    ? PackIconMaterialKind.WeatherNight 
+                    : PackIconMaterialKind.WeatherSunny;
+            }
+        }
 
-		private void Window_MouseDown(object sender, MouseButtonEventArgs e)
-		{
-			if (e.ChangedButton == MouseButton.Left)
-				this.DragMove();
-		}
-	}
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
+        }
+    }
 }
