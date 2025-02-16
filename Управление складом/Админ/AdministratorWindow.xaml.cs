4b581f323588329726e4d67147bcfc26560e827e
Максим Бойко <mboyko2005@gmail.com>
2025-02-16 16:15:52 +0500

    Исправлено множество ошибок.Добавлена функция QR

--- Управление складом/Админ/AdministratorWindow.xaml.cs
+++ Управление складом/Админ/AdministratorWindow.xaml.cs
@@ -5,44 +5,59 @@ using Управление_складом.Themes;
 
 namespace УправлениеСкладом
 {
-    public partial class AdministratorWindow : Window, IRoleWindow, IThemeable
-    {
-        public AdministratorWindow()
-        {
-            InitializeComponent();
-            UpdateThemeIcon();
-        }
+	public partial class AdministratorWindow : Window, IRoleWindow, IThemeable
+	{
+		public AdministratorWindow()
+		{
+			InitializeComponent();
+			UpdateThemeIcon();
+		}
 
-        public void ShowWindow() => Show();
+		public void ShowWindow()
+		{
+			Show();
+		}
 
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
         }
 
-        private void ManageUsers_Click(object sender, RoutedEventArgs e)
-        {
-            var usersWindow = new ManageUsersWindow { Owner = this };
-            usersWindow.ShowDialog();
-        }
+		private void ManageUsers_Click(object sender, RoutedEventArgs e)
+		{
+			ManageUsersWindow usersWindow = new ManageUsersWindow
+			{
+				Owner = this
+			};
+			usersWindow.ShowDialog(); // Открытие как модального окна
+		}
 
-        private void ManageInventory_Click(object sender, RoutedEventArgs e)
-        {
-            var inventoryWindow = new ManageInventoryWindow { Owner = this };
-            inventoryWindow.ShowDialog();
-        }
+		private void ManageInventory_Click(object sender, RoutedEventArgs e)
+		{
+			ManageInventoryWindow inventoryWindow = new ManageInventoryWindow
+			{
+				Owner = this
+			};
+			inventoryWindow.ShowDialog();
+		}
 
-        private void Reports_Click(object sender, RoutedEventArgs e)
-        {
-            var reportsWindow = new ReportsWindow { Owner = this };
-            reportsWindow.ShowDialog();
-        }
+		private void Reports_Click(object sender, RoutedEventArgs e)
+		{
+			ReportsWindow reportsWindow = new ReportsWindow
+			{
+				Owner = this
+			};
+			reportsWindow.ShowDialog();
+		}
 
-        private void Settings_Click(object sender, RoutedEventArgs e)
-        {
-            var settingsWindow = new SettingsWindow { Owner = this };
-            settingsWindow.ShowDialog();
-        }
+		private void Settings_Click(object sender, RoutedEventArgs e)
+		{
+			SettingsWindow settingsWindow = new SettingsWindow
+			{
+				Owner = this
+			};
+			settingsWindow.ShowDialog();
+		}
 
         private void ToggleTheme_Click(object sender, RoutedEventArgs e)
         {
@@ -50,22 +65,18 @@ namespace УправлениеСкладом
             UpdateThemeIcon();
         }
 
-        public void UpdateThemeIcon()
-        {
-            if (ThemeIcon != null)
-            {
-                ThemeIcon.Kind = ThemeManager.IsDarkTheme 
-                    ? PackIconMaterialKind.WeatherNight 
-                    : PackIconMaterialKind.WeatherSunny;
-            }
-        }
+		public void UpdateThemeIcon()
+		{
+			if (ThemeIcon != null)
+			{
+				ThemeIcon.Kind = ThemeManager.IsDarkTheme ? PackIconMaterialKind.WeatherNight : PackIconMaterialKind.WeatherSunny;
+			}
+		}
 
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ChangedButton == MouseButton.Left)
-            {
-                DragMove();
-            }
-        }
-    }
+		private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if (e.ChangedButton == MouseButton.Left)
+				this.DragMove();
+		}
+	}
 }
