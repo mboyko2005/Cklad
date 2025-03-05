9c25c9324fe7124bc4a4fec44e28d99b6b2a8cdc
Максим Бойко <mboyko2005@gmail.com>
2025-03-05 12:50:16 +0500

    Добавлена команда для сжатие в единый файл exe

--- Управление складом/App.xaml.cs
+++ Управление складом/App.xaml.cs
@@ -7,6 +7,10 @@ namespace Управление_складом
 	/// <summary>
 	/// Interaction logic for App.xaml
 	/// </summary>
+	/// </summary>
+	/// Для сжатие в единый exe файл
+	/// dotnet publish -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true -p:EnableCompressionInSingleFile=true -p:UseAppHost=true -p:PublishTrimmed=false -o publish
+	///<summary>
 	public partial class App : Application
 	{
 	}
